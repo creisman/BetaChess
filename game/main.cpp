f0ee358d6c95314c2a8971f9be47fd17f591e575
Seth Troisi <braintwo@gmail.com>
2017-01-08 18:56:09 -0800

    added main whoops

---
+++ game/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "board.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc > 1){
+    cout << "Called with " << argc << " args" << endl;
+  }
+
+  Board b(true /* init */);
+  int ply = 4;
+
+  int count = 0;
+  int captures = 0;
+  int mates = 0;
+  b.perft(ply, &count, &captures, &mates);
+
+  cout << "Perft results for" <<
+          " depth (ply): " << ply << endl;
+  cout << "\tcount: " << count << 
+          "\tcaptures: " << captures << 
+          "\tmates: " << mates << endl;
+
+
+  return 0;
+}
